43cc0f2ed0751b13b198f12de446fe89ee366d7d
Matt Wujek <mattwujek@gmail.com>
2015-12-10 14:57:05 -0800

    added project page [mobile first] with flickity plugin

--- js/script.js
+++ js/script.js
@@ -9,4 +9,29 @@ menuBtn.click(function() {
   navMenu.toggleClass('show-mobile-nav');
 });
 
+var workContent = $('.work-section');
+// workContent.toggleClass('hidden-section');
+$(".everlane-title").typed({
+    strings: ["Everlane Mobile"],
+    contentType: 'html',
+    typeSpeed: 50,
+    showCursor: false,
+     onStringTyped: function() {
+      console.log('done!');
+      setTimeout(function(){
+        workContent.removeClass('hidden-section');
+      },150);
+      
+     }
+});
+
+// flicker
+$('.project-gallery').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true,
+  imagesLoaded: true,
+  prevNextButtons: false
+});
+
 }); // end of ready
