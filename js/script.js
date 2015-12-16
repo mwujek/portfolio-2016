3d6b54ea5b6c3df8b46374310480fe2268981246
Matt Wujek <mattwujek@gmail.com>
2015-12-15 23:38:34 -0800

    added flickity RWD trick to project page (everlane)

--- js/script.js
+++ js/script.js
@@ -26,13 +26,78 @@ var workContent = $('.work-section');
 // });
 
 
-// waypoints ... get to work
-var workSection = $('#work');
-var waypoints = $('#work').waypoint({
-  handler: function(direction) {
-    console.log(direction + ' hit')
+// flickity function
+function globalFunction() {
+  //var windowW = $(window).width();
+  var breakPoint = 800;
+
+  function toggleFlicker() {
+    if (flickityActive) {
+      // destroy Flickity
+      $gallery.flickity('destroy');
+    } else {
+      // init new Flickity
+      $gallery.flickity({
+        prevNextButtons: false,
+        cellAlign: 'center',
+        contain: true
+      });
+    }
+    flickityActive = !flickityActive;
+    resizeObject.mobileView = !resizeObject.mobileView;
   }
-})
+
+  var resizeObject = {
+    mobileView: false,
+    breakWidth: breakPoint,
+    checkView: function() {
+      if ($(window).width() < this.breakWidth) {
+        this.mobileView = true;
+      } else {
+        this.mobileView = false;
+      }
+    }
+  };
+
+  // Set Gallery Variables
+  var flickityActive = true;
+  var $gallery = $('.project-gallery').each(function() {
+    $(this).flickity({
+      prevNextButtons: false,
+      cellAlign: 'center',
+      contain: true
+    });
+  });
+
+  // initiate gallery
+  resizeObject.checkView();
+  if (!resizeObject.mobileView) {
+    $gallery.flickity('destroy');
+    flickityActive = false;
+  }
+
+  $(window).resize(function() {
+    var currentW = $(this).width();
+    if (resizeObject.mobileView && currentW > resizeObject.breakWidth || !resizeObject.mobileView && currentW < resizeObject.breakWidth) {
+      toggleFlicker();
+    }
+  }); // end of resize
+}
+
+// everlane
+var href = window.location.pathname;
+if(href === '/everlane.html'){
+	globalFunction();
+}
+
+
+// waypoints ... get to work
+// var workSection = $('#work');
+// var waypoints = $('#work').waypoint({
+//   handler: function(direction) {
+//     console.log(direction + ' hit');
+//   }
+// });
 
 
 }); // end of ready
